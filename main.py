16527a23b34f1100aa484d60d78c1e2503d0a7d1
root <s.moosavi1990@gmail.com>
2022-03-18 14:27:14 -0400

    Add base structure for main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-from pathlib import Parh
+from pathlib import Path
+dir_path = "/opt/test_projec"
+dir_path = Path (dir_path)
+
+for item_path in dir_path.iterdir():
+    print(item_path.is_file())
+
+
+
